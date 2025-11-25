073ff3d33dcb0522de6538682c1d63a38a9adf64
Minkyu Cho <minkyu@greyboxhq.com>
2025-11-25 14:13:35 +0900

    chore: format

--- lib/src/services/storage_service.dart
+++ lib/src/services/storage_service.dart
@@ -5,6 +5,10 @@ import 'package:package_info_plus/package_info_plus.dart';
 import '../utils/logging/clix_logger.dart';
 
 class StorageService {
+  static const String _projectIdKey = 'clix_project_id';
+
+  static MMKV get _defaultMMKV => MMKV.defaultMMKV();
+
   late final MMKV _mmkv;
   bool _initialized = false;
 
@@ -19,17 +23,14 @@ class StorageService {
       }
 
       await MMKV.initialize(groupDir: groupDir, logLevel: MMKVLogLevel.Info);
+
+      _defaultMMKV.encodeString(_projectIdKey, projectId);
+
       _mmkv = MMKV('clix.$projectId');
       _initialized = true;
     } catch (e) {
       ClixLogger.error('Failed to initialize storage service', e);
       rethrow;
-    }
-  }
-
-  void _ensureInitialized() {
-    if (!_initialized) {
-      throw StateError('StorageService must be initialized before use');
     }
   }
 
@@ -57,5 +58,26 @@ class StorageService {
   Future<void> remove(String key) async {
     _ensureInitialized();
     _mmkv.removeValue(key);
+  }
+
+  void _ensureInitialized() {
+    if (!_initialized) {
+      throw StateError('StorageService must be initialized before use');
+    }
+  }
+
+  static Future<String?> getStoredProjectId() async {
+    try {
+      String? groupDir;
+      if (Platform.isIOS) {
+        final packageInfo = await PackageInfo.fromPlatform();
+        groupDir = 'group.clix.${packageInfo.packageName}';
+      }
+
+      await MMKV.initialize(groupDir: groupDir, logLevel: MMKVLogLevel.Info);
+      return _defaultMMKV.decodeString(_projectIdKey);
+    } catch (e) {
+      return null;
+    }
   }
 }
