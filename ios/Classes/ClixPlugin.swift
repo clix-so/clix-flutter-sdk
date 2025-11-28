3dd2a029767bd6f9beba0a586e453b7054208504
Minkyu Cho <minkyu@greyboxhq.com>
2025-11-28 16:58:02 +0900

    feat: add ClixFlutterApi for notification tap handling on iOS and Android
    - Implement ClixFlutterApi in generated Kotlin and Swift code to enable
     Flutter-to-native communication for notification tap events
    - Add ClixNativeBridge in Dart to handle onNotificationTapped and route
     payloads to NotificationService
    - Update iOS plugin to forward notification tap events to Flutter via
     ClixFlutterApi
    - Refactor notification presentation logic for better Clix payload handling
    - Add error handling for channel connection failures in generated code
    - Update Dart message handler to support onNotificationTapped and
     standardized response wrapping
    
    These changes enable the app to handle notification tap events natively
    and forward them to Flutter, improving cross-platform notification
    interactivity and reliability.
    
    feat(notification_service): integrate ClixFlutterApi for iOS native bridge
    refactor(notification_service): rename _handleNotificationTap to handleNotificationTap for public access
    feat(messages): add ClixFlutterApi with onNotificationTapped for Flutter-native communication

--- ios/Classes/ClixPlugin.swift
+++ ios/Classes/ClixPlugin.swift
@@ -7,21 +7,19 @@ extension FlutterError: Error {}
 public class ClixPlugin: NSObject, FlutterPlugin, ClixHostApi {
 
     private var previousDelegate: UNUserNotificationCenterDelegate?
+    private var flutterApi: ClixFlutterApi?
 
     public static func register(with registrar: FlutterPluginRegistrar) {
         let instance = ClixPlugin()
+        instance.flutterApi = ClixFlutterApi(binaryMessenger: registrar.messenger())
         ClixHostApiSetup.setUp(binaryMessenger: registrar.messenger(), api: instance)
         registrar.addApplicationDelegate(instance)
 
         DispatchQueue.main.async {
-            let center = UNUserNotificationCenter.current()
-            instance.previousDelegate = center.delegate
-            center.delegate = instance
+            let notificationCenter = UNUserNotificationCenter.current()
+            instance.previousDelegate = notificationCenter.delegate
+            notificationCenter.delegate = instance
         }
-    }
-
-    private func isClixMessage(_ userInfo: [AnyHashable: Any]) -> Bool {
-        userInfo["clix"] != nil
     }
 }
 
@@ -33,17 +31,25 @@ extension ClixPlugin: UNUserNotificationCenterDelegate {
         withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
     ) {
         let userInfo = notification.request.content.userInfo
+        let isLocalNotification = !(notification.request.trigger is UNPushNotificationTrigger)
+        let hasClixPayload = userInfo["clix"] != nil
 
-        if isClixMessage(userInfo) {
-            completionHandler([])
+        if isLocalNotification {
+            completionHandler([.banner, .sound, .badge])
             return
         }
 
-        if let previousDelegate = previousDelegate,
-           previousDelegate.responds(to: #selector(userNotificationCenter(_:willPresent:withCompletionHandler:))) {
-            previousDelegate.userNotificationCenter?(center, willPresent: notification, withCompletionHandler: completionHandler)
+        if let delegate = previousDelegate,
+           delegate.responds(to: #selector(userNotificationCenter(_:willPresent:withCompletionHandler:))) {
+            if hasClixPayload {
+                delegate.userNotificationCenter?(center, willPresent: notification) { _ in
+                    completionHandler([])
+                }
+            } else {
+                delegate.userNotificationCenter?(center, willPresent: notification, withCompletionHandler: completionHandler)
+            }
         } else {
-            completionHandler([.banner, .sound, .badge])
+            completionHandler(hasClixPayload ? [] : [.banner, .sound, .badge])
         }
     }
 
@@ -52,19 +58,16 @@ extension ClixPlugin: UNUserNotificationCenterDelegate {
         didReceive response: UNNotificationResponse,
         withCompletionHandler completionHandler: @escaping () -> Void
     ) {
-        if let previousDelegate = previousDelegate,
-           previousDelegate.responds(to: #selector(userNotificationCenter(_:didReceive:withCompletionHandler:))) {
-            previousDelegate.userNotificationCenter?(center, didReceive: response, withCompletionHandler: completionHandler)
+        let userInfo = response.notification.request.content.userInfo
+        let userInfoDict = userInfo.reduce(into: [String?: Any?]()) { $0[$1.key as? String] = $1.value }
+        flutterApi?.onNotificationTapped(userInfo: userInfoDict) { _ in }
+
+        if let delegate = previousDelegate,
+           delegate.responds(to: #selector(userNotificationCenter(_:didReceive:withCompletionHandler:))) {
+            delegate.userNotificationCenter?(center, didReceive: response, withCompletionHandler: completionHandler)
         } else {
             completionHandler()
         }
     }
 }
 
-extension ClixPlugin: UIApplicationDelegate {
-    public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    }
-
-    public func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-    }
-}
